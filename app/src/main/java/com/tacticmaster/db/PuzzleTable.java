2689ed8d5c96aabcce6fbe59fe0160896d074615
d050150 <tibor.tarnai@sap.com>
2025-04-28 12:43:11 +0200

    reduce puzzle db size

--- app/src/main/java/com/tacticmaster/db/PuzzleTable.java
+++ app/src/main/java/com/tacticmaster/db/PuzzleTable.java
@@ -7,12 +7,6 @@ public class PuzzleTable {
     public static final String COLUMN_FEN = "FEN";
     public static final String COLUMN_MOVES = "Moves";
     public static final String COLUMN_RATING = "Rating";
-    public static final String COLUMN_RATING_DEVIATION = "RatingDeviation";
-    public static final String COLUMN_POPULARITY = "Popularity";
-    public static final String COLUMN_NB_PLAYS = "NbPlays";
-    public static final String COLUMN_THEMES = "Themes";
-    public static final String COLUMN_GAME_URL = "GameUrl";
-    public static final String COLUMN_OPENING_TAGS = "OpeningTags";
     public static final String COLUMN_SOLVED = "Solved";
 }
 
--- app/src/main/java/com/tacticmaster/puzzle/Puzzle.java
+++ app/src/main/java/com/tacticmaster/puzzle/Puzzle.java
@@ -4,13 +4,7 @@ public record Puzzle(
         String puzzleId,
         String fen,
         String moves,
-        int rating,
-        int ratingDeviation,
-        int popularity,
-        int nbPlays,
-        String themes,
-        String gameUrl,
-        String openingTags
+        int rating
 ) implements Comparable<Puzzle> {
 
     @Override
